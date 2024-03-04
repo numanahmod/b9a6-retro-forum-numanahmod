73319213da1f3b2bfacbb6fdd3e27ac957ad2450
Your Name <you@example.com>
2024-03-04 22:35:20 +0600

    Alhamdulillah mark read section is done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
+
+let allPost = []
+
 const loadCard = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
     const data = await res.json();
     const cards = data.posts;
     // console.log(cards);
+    allPost = cards;
     displayCards(cards);
+    // displayTitleCards(titleCards);
 }
 
 const displayCards = cards => {
@@ -26,7 +31,7 @@ const displayCards = cards => {
             <p> # <span>${cards.category}</span>   Author: <span>${cards.author.name}</span></p>
             <h1 class="card-title"> <span> ${cards.title}</span> </h1>
             <p class="card-text"> <span> ${cards.description}</p>
-            <hr class="mt-6">
+            <hr class="mt-6 w-[596px]">
             <div class="flex grid-cols-2 justify-between">
               <div><div class="mt-10 flex grid-cols-3 gap-10"> 
                 <div class="flex grid-cols-2 gap-3"> <div><svg width="22.500000" height="21.333374" viewBox="0 0 22.5 21.3334" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -50,11 +55,11 @@ const displayCards = cards => {
                </div> 
               </div>
               <div class="mt-10">
-                <svg width="27.999817" height="28.000000" viewBox="0 0 27.9998 28" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+               <button id="mark" onclick="buttonHandler('${cards.title}','${cards.view_count}')" class="read-button"> <p></p> <svg width="27.999817" height="28.000000" viewBox="0 0 27.9998 28" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                
                   <defs/>
                   <path id="Vector" d="M13.9997 0C6.26794 0 0 6.26819 0 13.9999C0 21.7314 6.26794 28 13.9997 28C21.7314 28 27.9998 21.7314 27.9998 13.9999C27.9998 6.26819 21.7314 0 13.9997 0ZM13.9999 4.91736L22.2846 10.0835L5.71533 10.0835L13.9999 4.91736ZM22.3878 18.333L22.387 18.333C22.387 19.1616 21.7154 19.833 20.8869 19.833L7.11301 19.833C6.28439 19.833 5.61295 19.1615 5.61295 18.333L5.61295 10.4122C5.61295 10.3246 5.62189 10.2394 5.63644 10.1556L13.5519 15.0914C13.5616 15.0974 13.572 15.1016 13.582 15.1072C13.5925 15.1129 13.6031 15.1185 13.6137 15.1239C13.6696 15.1527 13.7272 15.176 13.7861 15.1912C13.7922 15.1929 13.7982 15.1936 13.8043 15.1949C13.8689 15.2102 13.9343 15.2197 13.9997 15.2197L14.0002 15.2197C14.0006 15.2197 14.0011 15.2197 14.0011 15.2197C14.0664 15.2197 14.1318 15.2104 14.1964 15.1949C14.2025 15.1935 14.2086 15.1929 14.2146 15.1912C14.2734 15.176 14.3308 15.1527 14.387 15.1239C14.3976 15.1185 14.4083 15.1129 14.4187 15.1072C14.4286 15.1016 14.4391 15.0974 14.4488 15.0914L22.3643 10.1556C22.3788 10.2394 22.3878 10.3243 22.3878 10.4122L22.3878 18.333Z" fill="#10B981" fill-opacity="1.000000" fill-rule="nonzero"/>
-                </svg>
+                </svg> </button> 
               </div>
             </div>
           </div>
@@ -63,5 +68,109 @@ const displayCards = cards => {
             cardContainer.appendChild(cardDiv);
         })
 }
+
+// Handle search button 
+const handleSearch = ( ) => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+
+    console.log(searchText)
+}
+
+// title mark 
+// const displayTitleCards = titleCards => {
+//   // console.log(cards);
+//   // step - 1 
+//   const cardContainer = document.getElementById('card-container');
+//   titleCards.forEach(titleCards => {
+//       console.log(titleCards);
+//       // step-2 create div
+//       const cardDiv =document.createElement('div');
+//       // cardDiv.classList = `flex grid-cols-2 card-1 gap-5 shadow-xl gap-8`;
+//       // step 3 inner html
+//       cardDiv.innerHTML = `
+//       <div class="title-cards flex grid-cols-2 justify-between -ml-3"> 
+//          <div> <h1 class="text-sm ml-2 mt-4"> 10 Kids Unaware of Their Halloween Costume</h1> </div>
+//           <div class="ml-2 mt-4">
+//             <p class="inline-flex gap-2"><svg width="22.500000" height="15.500000" viewBox="0 0 22.5 15.5" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        
+//               <defs/>
+//               <path id="Vector" d="M9.6001 9.3999C10.0377 9.83752 10.6312 10.0834 11.25 10.0834C11.8688 10.0834 12.4623 9.83752 12.8999 9.3999C13.3375 8.96228 13.5833 8.3689 13.5833 7.75C13.5833 7.1311 13.3375 6.5376 12.8999 6.1001C12.4623 5.6626 11.8688 5.41675 11.25 5.41675C10.6312 5.41675 10.0377 5.6626 9.6001 6.1001C9.16248 6.5376 8.91669 7.1311 8.91669 7.75C8.91669 8.3689 9.16248 8.96228 9.6001 9.3999ZM11.25 14.75C7.04999 14.75 3.54999 12.4166 0.75 7.75C3.54999 3.08325 7.04999 0.75 11.25 0.75C15.45 0.75 18.95 3.08325 21.75 7.75C18.95 12.4166 15.45 14.75 11.25 14.75Z" stroke="#12132D" stroke-opacity="0.600000" stroke-width="1.500000" stroke-linejoin="round"/>
+//             </svg>
+//             <span class="-mt-1 text-sm mr-2">1,568</span>
+//             </p>
+  
+//           </div>
+//         </div>
+// `;
+//   // step 4 append child 
+//       cardContainer.appendChild(cardDiv);
+//   })
+  
+// }
+let count =0;
+const buttonHandler = (title,view_count) => {
+  // console.log(cards)
+  // console.log(view_count)
+  const currentNumber =document.getElementById('read-count');
+          count = count +1;
+          currentNumber.innerText = count;
+// const readTitle = document.getElementById('mark-read');
+// readTitle.innerText = title;
+
+const titleCardContainer = document.getElementById('mark-read');
+        // allPost.forEach(post => {
+            // console.log(id);
+            // step-2 create div
+            const titleCardDiv =document.createElement('div');
+            // cardDiv.classList = `flex grid-cols-2 card-1 gap-5 shadow-xl gap-8`;
+            // step 3 inner html
+            titleCardDiv.innerHTML = `
+            <div class="title-cards flex grid-cols-2 justify-between -ml-3 mb-3"> 
+           <div> <h1 class="text-sm ml-2 mt-4"> ${title} </h1> </div>
+            <div class="ml-2 mt-4">
+              <p class="inline-flex gap-2"><svg width="22.500000" height="15.500000" viewBox="0 0 22.5 15.5" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          
+                <defs/>
+                <path id="Vector" d="M9.6001 9.3999C10.0377 9.83752 10.6312 10.0834 11.25 10.0834C11.8688 10.0834 12.4623 9.83752 12.8999 9.3999C13.3375 8.96228 13.5833 8.3689 13.5833 7.75C13.5833 7.1311 13.3375 6.5376 12.8999 6.1001C12.4623 5.6626 11.8688 5.41675 11.25 5.41675C10.6312 5.41675 10.0377 5.6626 9.6001 6.1001C9.16248 6.5376 8.91669 7.1311 8.91669 7.75C8.91669 8.3689 9.16248 8.96228 9.6001 9.3999ZM11.25 14.75C7.04999 14.75 3.54999 12.4166 0.75 7.75C3.54999 3.08325 7.04999 0.75 11.25 0.75C15.45 0.75 18.95 3.08325 21.75 7.75C18.95 12.4166 15.45 14.75 11.25 14.75Z" stroke="#12132D" stroke-opacity="0.600000" stroke-width="1.500000" stroke-linejoin="round"/>
+              </svg>
+              <span class="-mt-1 text-sm mr-2">${view_count}</span>
+              </p>
+    
+            </div>
+          </div>`;
+        // step 4 append child 
+            titleCardContainer.appendChild(titleCardDiv);
+        // })
+
+
+
+
+
+
+
+  // const currentPost = allPost.find((post)=> post.id == id)
+  // // console.log(currentPost)
+  // 
+      
+  //       const reads = document.querySelectorAll(".read-button")
+
+  //       for (let index = 0; index < reads.length; index++) {
+  //         const read = reads[index];
+  //         read.addEventListener("click", function readCount(){
+  //           const currentNumber =document.getElementById('read-count');
+  //         const currentText = currentNumber.innerText;
+  //         const currentRead =parseInt(currentText);
+  //         const newBooking = currentRead +1;
+  //         currentNumber.innerText = newBooking;
+  //         })
+          
+  //       }
+        
+}
+// spinner 
+
+
+
 
 loadCard()
--- js/script2.js
+++ js/script2.js
@@ -35,7 +35,7 @@ const displayLatestCards = cards => {
               <path id="Vector" d="M11.2929 16.7071C11.4804 16.8947 11.7348 17 12 17C12.2652 17 12.5196 16.8947 12.7071 16.7071C12.8946 16.5196 13 16.2652 13 16C13 15.7348 12.8946 15.4804 12.7071 15.2929C12.5196 15.1053 12.2652 15 12 15C11.7348 15 11.4804 15.1053 11.2929 15.2929C11.1054 15.4804 11 15.7348 11 16C11 16.2652 11.1054 16.5196 11.2929 16.7071Z" stroke="#12132D" stroke-opacity="0.600000" stroke-width="1.500000" stroke-linejoin="round"/>
             </g>
           </svg>
-          <span>${cards.author.posted_date? cards.author.posted_date: "No Published Date" } </span></p>
+          <span class="latest-text">${cards.author.posted_date? cards.author.posted_date: "No Published Date" } </span></p>
           <h3 class="latest-title">${cards.title}</h3>
           <p class="latest-text">${cards.description} </p>
           <div class="flex grid-cols-2 mt-5"> 
